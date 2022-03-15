f5f91e62c0c42c4f272a20402c8b69c6c9b263cb
FabienNaveau <fabien.naveau@gmail.com>
2022-03-15 17:28:33 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Home from "../src/components/Home"
+
+function App() {
+  return (
+    <>
+    <Home />
+    <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">Fabien Naveau</a>.
+    </div>
+    </>
+    
+  );
+}
+
+export default App;
