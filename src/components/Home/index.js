6272271961ad6c6a586969a7a9d3bee7109a0671
FabienNaveau <fabien.naveau@gmail.com>
2022-03-15 20:03:49 +0100

    ajout de l'input pour recupérer la valeur de l'ip

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function Home () {
+
+    const [ipAddress, setIpAddress] = useState("");
+    const getInputValue = () => {
+        console.log("Mafonction fonctionne")
+    }
     return (
-        
-        <h1>Hello World</h1>
-        
+        <>
+        <div className="Ip-form">
+            <input type="text" name="ipadress" id="ipInput" placeholder="Search for any IP address" />
+            <button type="submit" onClick={getInputValue}><img src="/../../public/images/icon-arrow.svg" alt="bouton de validation" /></button>
+        </div>
+        </>
     )
 }
