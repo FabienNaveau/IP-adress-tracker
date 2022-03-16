40e63a02da26746955bc73d1f077d23346ae665f
FabienNaveau <fabien.naveau@gmail.com>
2022-03-16 16:51:45 +0100

    Ajout de la requete permettant de recuperer addresse IP

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
     <Home />
-    <div class="attribution">
+    <div className="attribution">
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
     Coded by <a href="#">Fabien Naveau</a>.
     </div>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,32 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
+import axios from "axios"
+import arrow from "../../images/icon-arrow.svg";
 
 export default function Home () {
 
     const [ipAddress, setIpAddress] = useState("");
-    const getInputValue = () => {
-        console.log("Mafonction fonctionne")
+
+
+
+    const getInputValue = (event) => {
+        event.preventDefault();
+        setIpAddress(document.querySelector("#ipInput").value);
+        console.log(ipAddress)
     }
+
+    const getIpInformations = async () => {
+        const res = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=at_KKLn7FwoBSlOdEE3TlBpJDZiCca74&ipAddress=${ipAddress}`)
+        console.log(res.data)
+        
+    }
+
+    
+    
     return (
         <>
         <div className="Ip-form">
             <input type="text" name="ipadress" id="ipInput" placeholder="Search for any IP address" />
-            <button type="submit" onClick={getInputValue}><img src="/../../public/images/icon-arrow.svg" alt="bouton de validation" /></button>
+            <button type="submit" onClick={getInputValue}><img src={arrow} alt="Flêche permettant de soumettre l'input" /></button>
         </div>
         </>
     )
