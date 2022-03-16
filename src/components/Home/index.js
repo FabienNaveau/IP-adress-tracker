52c7a9b0716b3d6ad0bf6899ba35ec047fadd4f0
FabienNaveau <fabien.naveau@gmail.com>
2022-03-16 19:57:34 +0100

    affichage des données suite à la reception de la reponse API

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,32 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios"
 import arrow from "../../images/icon-arrow.svg";
 
 export default function Home () {
 
     const [ipAddress, setIpAddress] = useState("");
+    const [timeZone, setTimezone] = useState("")
+    const [country, setCountry] = useState("")
+    const [lat, setLat] = useState("")
+    const [long, setLong] = useState("")
+    const [asn, setAsn] = useState("")
+    const [isp, setIsp] = useState("")
+    const [error, setError] = useState("")
 
-
-
-    const getInputValue = (event) => {
+    const getIpInformations = async (event) => {
         event.preventDefault();
         setIpAddress(document.querySelector("#ipInput").value);
-        console.log(ipAddress)
-    }
-
-    const getIpInformations = async () => {
+        console.log("mon adresse Ip :", ipAddress)
         const res = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=at_KKLn7FwoBSlOdEE3TlBpJDZiCca74&ipAddress=${ipAddress}`)
-        console.log(res.data)
-        
-    }
+        console.log(res.status)
+        if(res.status == 200) {
+            const location = res.data.location
+            setTimezone(location.timezone)
+            setCountry(location.country)
+            setLat(location.lat)
+            setLong(location.lng)
+            setAsn(res.data.as.asn)
+            setIsp(res.data.isp)
+        } else {
+            setError("Désolé mais l'adresse entrée n'existe pas ou est privée")
+        }
 
-    
+    }
+    const afficherIpInfos = () => {
+        if(country) {
+            return(
+                <p>{timeZone}, {country}, {lat}, {long}, {asn}, {isp}</p>
+            )
+        } else {
+            return (
+            <p>{error}</p>
+            )
+        }
+    }
     
     return (
         <>
         <div className="Ip-form">
             <input type="text" name="ipadress" id="ipInput" placeholder="Search for any IP address" />
-            <button type="submit" onClick={getInputValue}><img src={arrow} alt="Flêche permettant de soumettre l'input" /></button>
+            <button type="submit" onClick={getIpInformations}><img src={arrow} alt="Flêche permettant de soumettre l'input" /></button>
+        </div>
+        <div>
+            {afficherIpInfos()}
         </div>
         </>
     )
